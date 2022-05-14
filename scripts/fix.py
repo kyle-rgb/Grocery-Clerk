ca07e214e8d8015dce3c64ae65eb17ae71be7118
Kyle Lewis <kylel9815@gmail.com>
2022-05-13 23:45:32 -0400

    Added Parsed April 20th Cashback JSON Files to Archive and Finished fix.py

--- scripts/fix.py
+++ scripts/fix.py
@@ -4,6 +4,9 @@ from pprint import pprint
 from multiprocessing import Pool
 from tracemalloc import start
 
+from cv2 import Mat
+from numpy import append
+
 def fixData():
 
     with open('./requests/server/collections/toFix/aprilTrips.txt', 'r', encoding="utf-8") as file:
@@ -71,6 +74,18 @@ def partitionString(string, openChar, closeChar, index=0):
                 
     print(arr)
 
+def canItBe(func, error, stringMain, testStrings):
+    test_cases = [stringMain+x for x in testStrings]
+    print([stringMain[-100:]+"<TESTCASES>"+x[:100] for x in testStrings])
+    r = []
+    for t in test_cases:
+        try:
+
+            func(t)
+            r.append(True)
+        except error as e:
+            r.append(False)
+    return r
 
 def forceClose(dataFile):
     # identifys loose objects involuntarily inserted into streams via early pulls
@@ -135,12 +150,17 @@ def forceClose(dataFile):
     objects = list(filter(lambda x: ((x!='')and(re.match(regURL, x))==None), urlsAndObjects))
     filteredObjects = []
     k=0
+    enders = []
+    oS = []
+    eS = []
+
 
     print("URLS:", len(urls), "OBJECTS:", len(objects))
     for i, o in enumerate(objects):
         completeObjectRegex = re.compile(r"(,\s)(?=\{\"(data|success)\")") # check to see if object is complete and duplicated
         brokenObjectRegex = re.compile(r"(?<!,\s)(\{\"(data|success)\")")
         completeObjects = re.split(completeObjectRegex, o)
+
         if len(completeObjects)==1:
             # object broken by stream, we know that the stream breakpoint is the start of the original string
             # search nonsplit string for its beginning, get both the span to find break location
@@ -150,12 +170,12 @@ def forceClose(dataFile):
             endOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', o[-500:])
             starts = [x.span() for x in re.finditer(startOfStream, o)]
             ends = [x.span() for x in re.finditer(endOfStream, o)]
+            #print(o[ends[0][1]:ends[0][1]+100])
             if len(starts)!=2 or len(ends)!=2:
                 print(starts, ends)
                 raise re.error('indicators contain an unescaped general character')
             
             cutoff = o[:starts[1][0]]
-            print(len(cutoff))
             s1 = o[starts[0][0]:starts[1][0]] + o[ends[0][1]+1:] 
             s2 = o[starts[1][0]:ends[0][1]]
             a = max([s1, s2], key=len)
@@ -176,34 +196,116 @@ def forceClose(dataFile):
                     lastStart = stop
             newString = newString + a[lastStart:]
             try:
-                json.loads(newString)
+                filteredObjects.append(json.loads(newString))
+                
             except json.decoder.JSONDecodeError as e:
+
                 if e.pos == len(newString)-1:
                     try:
                         print(e)
-                        print(newString[-20:])
-                        json.loads(newString[:-4])
+                        print(newString[-200:])
+                        print(newString[:200])
+                        json.loads(newString)
+
                     except json.decoder.JSONDecodeError as je:
                         print(je)
+                        with open('g.json', 'w', encoding='utf-8') as h:
+                            h.write(newString)
+                        
                 else:
-                    #print(f"{newString[e.pos-1000:e.pos]}" + f"<<<<<{newString[e.pos]}>>>>>",  f"{newString[e.pos+1:e.pos+100]}" )
                     msg = e.args[0]
-                    charAt = int(re.findall(r'char (\d+)', msg)[0])
-                    returningString = newString[e.pos+1:e.pos+500]
-                    startOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', returningString)
-                    # print([x.span() for x in re.finditer(startOfStream, newString[charAt:])])
-                    endOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', b[-500:])
-                    matches = [x.span() for x in re.finditer(endOfStream, newString)]
-                    matches = [re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1',newString[x[1]:x[1]+500])for x in matches]
+                    charAt = e.pos
                     
-                    matches = [x.span() for x in re.finditer(matches[0], newString)]
-                    print('\n')
-
-
-
-
+                    endOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', newString[e.pos:e.pos+1000])
+                    mat = [x.span() for x in re.finditer(endOfStream, newString[:e.pos])]
+                    eos = newString[e.pos:e.pos+1000] 
+                    eos = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', eos)
+                    mat = [x.span() for x in re.finditer(eos, newString)]
+                    returnal = b[-100:] + newString[mat[0][0]:mat[0][1]]
+                    unstreamed = newString[mat[1][0]-100:mat[1][1]]
+                    n=1
+                    while returnal not in unstreamed:
+                        returnal = b[-100+n:] + newString[charAt-n:][:100]
+                        n +=1
+                        if n > 50:
+                            break
                     
+                    if n == 51:
+                        file = dataFile.replace('.txt', '')
+                        file = file.replace('./requests/server/collections/toFix/', '')
+                        with open(f'./requests/server/collections/toFix/fixes/{file}ToFix.txt', 'w', encoding='utf-8') as f:
+                            f.write(f"<NEWSTRING{i}>")
+                            f.write(newString[:charAt])
+                            f.write(f"<ORIG_OBJECT{i}>")
+                            f.write(cutoff)
+                            f.write(f"<S1{i}>")
+                            f.write(s1)
+                            f.write(f"<S2{i}>")
+                            f.write(s2)
+                            f.write('<END_OF_STRING>')
+                    else:
+                        try:
+                            n-=1
+                            secondTry = b + newString[charAt-n:]
+                            json.loads(secondTry)
+                            print('passed 2')
+                        except json.decoder.JSONDecodeError as e2:
+                            print(e2)
+                            print(f"{secondTry[e2.pos-500:e2.pos]}" + f"<<<<<{secondTry[e2.pos]}>>>>>",  f"{secondTry[e2.pos+1:e2.pos+500]}" )
+                            
+                    
+                    
+                    
+                    #print([(newString[x[0]-100:x[0]], f"<<{newString[x[0]:x[1]]}>>", newString[x[1]:x[1]+100]) for x in mat])
+                   
+                    # while mat == []:
+                    #     charAt-= 1
+                    #     eos = b[-100:] + newString[charAt:charAt+100]
+                    #     print(eos)
+                    #     eos = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', eos)
+                    #     mat = [x.span() for x in re.finditer(eos, newString[:e.pos])]
+                        
+                    # print(mat)
+                    # returningString = newString[e.pos+1:e.pos+500]
+                    # startOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', returningString)
+                    # # print([x.span() for x in re.finditer(startOfStream, newString[charAt:])])
+                    # endOfStream = re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1', b[-500:])
+                    # matches = [x.span() for x in re.finditer(endOfStream, newString)]
+                    # matches = [re.sub(r'([\"\{\}\[\]\(\).$])', r'\\\1',newString[x[1]:x[1]+500])for x in matches]
+                    # matches = [x.span() for x in re.finditer(matches[0], newString)]
+                    #print("\n", matches)
+            print('#'*50, f'object {i}', f'length: {len(o)}', '#'*50)
+            print('\n')
 
+        else:
+            try:
+                filteredObjects.append(json.loads(completeObjects[0]))
+            except json.decoder.JSONDecodeError as e2:
+                secondTry = completeObjects[0]
+                lastStart = 0
+                newString = ''
+                for group in re.finditer(regFive, secondTry):
+                    groupMatches = group.groups()
+                    if '"' in groupMatches[1]:
+                        start, stop = group.span()
+                        data = re.sub(r'(?<![\\])"', " ",groupMatches[1])
+                        newString = newString + secondTry[lastStart:start] + groupMatches[0] + data
+                        lastStart = stop
+                    else:
+                        start, stop = group.span()
+                        newString = newString + secondTry[lastStart:stop] 
+                        lastStart = stop
+                newString = newString + secondTry[lastStart:]
+                try:
+                    filteredObjects.append(json.loads(newString))
+                except json.decoder.JSONDecodeError as e3:
+                    print(i, e3)
+
+    newFile = dataFile.split("/")
+    fileName = newFile[-1].replace('.txt', '.json')
+    newFile = "/".join(newFile[:-1]+["fixes"]+[fileName])
+    with open(newFile, 'w', encoding='utf-8') as final:
+        final.write(json.dumps(filteredObjects))
 
         # else: 
         #     # print(completeObjects[0]==completeObjects[-1])
