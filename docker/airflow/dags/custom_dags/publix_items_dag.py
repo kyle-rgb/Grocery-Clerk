6f1701ac3c288c307642247ee141e695d1434c7e
Kyle Lewis <kylel9815@gmail.com>
2022-11-04 17:24:19 -0400

    Removed Sample Dags and Updated All Scraping Dags to Monitor Runs Status, Interface with the Database, and Run in Own Container On Eachs Proper Scheduling Interval

--- docker/airflow/dags/custom_dags/publix_items_dag.py
+++ docker/airflow/dags/custom_dags/publix_items_dag.py
@@ -19,62 +19,123 @@ with DAG(
     catchup=False,
     tags=["grocery", "GroceryClerk", "ETL", "python", "node", "mongodb", "docker"]
 ) as dag:
-    # [START docker_wakeup_call]
-    @task(task_id="docker_wakeup_call")
-    def docker_wakeup_call():
-        # migrated current docker compose file (sans secrets) to replicate current compose network
+    # [START db_try]
+    @task(task_id="start_container_example")
+    def start_container():
         import docker
         client = docker.from_env()
-        client.containers.run("docker-scraper-1", "node", tty=True, ports={"8081/tcp": "8080", "9229/tcp": "9229", "5900/tcp": "5900", "5000/tcp": "5000"},
-        enviroment={"GPG_TTY": "/dev/pts/0", "DISPLAY": ":1", "XVFB_RESOLUTION": "1920x1080x16"},
-        init=True, stdin_open=True, mounts=[
-            docker.types.Mount("/app/node_modules", "browser_dependencies"),
-            docker.types.Mount("/tmp/collections", "./tmp/collections/", type="bind"),
-            docker.types.Mount("/app", "./scraper", type="bind")
-        ], detach=True)
+
+        container = client.containers.run("docker-scraper:latest", working_dir='/app', detach=True, name="scraper",
+                ports={"8081/tcp": "8081", "9229/tcp": "9229", "5900/tcp": "5900", "5000/tcp": "5000"},
+                environment={"GPG_TTY": "/dev/pts/0", "DISPLAY": ":1", "XVFB_RESOLUTION": "1920x1080x16"},
+                init=True, stdin_open=True
+            )
 
         return 0 
-    # [END docker_wakeup_call]
-    wakeUpTask = docker_wakeup_call()
+    # [END db_try]
 
-    # [START kroger_operator_setup_node]
-    @task(task_id="setup_browser_and_extract_data")
-    def setup_browser_and_extract(ds=None, **kwargs):
-        """Starts the Browser and Runs Node.js Setup in Container"""
-        # has to connect to docker network running on host via docker module 
-        # connect to docker and poke awake container; airflow worker should have docker module installed and access to host socket via volume
-        # TODO: DOCKER_HOST env variable will be used in later versions for ssh connections to remote container instances of scraper container network.
-        # run setUpBrowser from app/src/index.js with proper task argument
-        # wait for confirmation of task completion, forward any logging to airflow logs
-        # mark as success and move on to next task
+    @task(task_id="insert-run")
+    def insertRun(functionName=None, description=None, args=None, ti=None):
+        import docker
+        from airflow.secrets.local_filesystem import load_connections_dict
+        from pprint import pprint
+        import json 
+
+        connections = load_connections_dict("/run/secrets/secrets-connections.json")
+
+        client = docker.from_env()
+        container = client.containers.get("scraper")
+        baseCmd = "node ./src/db.js insert -c runs -e airflow"
+        if functionName:
+            baseCmd += " -f " + functionName
+        if description:
+            baseCmd += f" -d '{description}'"
+        if not args:
+            args = {}
+
+        args["pre_execute_stats"] = container.stats(stream=False)
+        baseCmd += f" --args '{json.dumps(args)}'"
+        print("executing $ ", baseCmd)
+        code, output = container.exec_run(cmd=baseCmd,
+            user="pptruser", environment={"MONGO_CONN_URL": connections["MONGO_CONN_URL"].get_uri()},
+            workdir="/app"
+        )
+        output = output.decode("ascii")
+        print(output)
+        output = re.findall(r"id=([a-f0-9]+)", output)[0]
+        ti.xcom_push(key="run_object_id", value=output)
+        return 0
+
+    @task(task_id="update-run")
+    def updateRun(functionName=None, args=None, push=False, description=None, ti=None):
+        import docker
+        from airflow.secrets.local_filesystem import load_connections_dict
+        from pprint import pprint
+        import json 
+        connections = load_connections_dict("/run/secrets/secrets-connections.json")
+
+        client = docker.from_env()
+        container = client.containers.get("scraper")
+        _id = ti.xcom_pull(key="run_object_id", task_ids="insert-run")
+        # at base level closes the loop on the returning function, then consults kwargs on whether to add to run stack 
+        stats = json.dumps(container.stats(stream=False))
+        baseCmd = f"node ./src/db.js insert -c runs -e airflow -i {_id} -s '{stats}'"
+        
+        if push:
+            baseCmd += f" -f {functionName} -d '{description}' --args '{json.dumps(args)}' --push"
+        print("executing $ ", baseCmd)
+        code, output = container.exec_run(cmd=baseCmd,
+        user="pptruser", environment={"MONGO_CONN_URL": connections["MONGO_CONN_URL"].get_uri()},
+        workdir="/app")
+        output = output.decode("ascii")
+        print(output)
+        return 0
+
+    @task(task_id="scrape_dataset")
+    def scrape_dataset(chain=None, target_data=None, add_args=None):
+        import docker
+        connections = load_connections_dict("/run/secrets/secrets-connections.json")
+
+        client = docker.from_env()
+        container = client.containers.get("scraper")
+        if chain=="kroger" and target_data=="promotions" and add_args:
+            target_data += add_args
+        code, output = container.exec_run(f"node ./src/index.js scrape --{chain} {target_data}", workdir="/app", user="pptruser")
+        output = output.decode("ascii")
+        print(output)
+
+        return 0
+
+    @task(task_id="setTimeout")
+    def setTimeout(to):
+        time.sleep(to)
+
+        return 0
+
+    @task(task_id="run-command")
+    def executeCommand(data=4500):
         import docker
         client = docker.from_env()
-        scrapingContainer = list(filter(lambda x: x.name=="docker-scraper-1"), client.containers.list())[0]
-        scrapingContainer.exec_run("node ./src/index.js --publix items", user="pptruser", workdir="/app")
-        return 0
-    # [END kroger_operator_setup_node]
-    extractTask = setup_browser_and_extract()
+        exit_code, output = client.containers.get("scraper").exec_run(f'node ./src/db.js test -d {data}', workdir="/app")
+        output = output.decode('ascii')
+        print(output)
+        return exit_code
 
-    # [START kroger_operator_setup_node]
-    @task(task_id="transform_items_data_publix")
-    def transform_publix_item_data(ds=None, **kwargs):
-        """Starts the Browser and Runs Node.js Setup in Container"""
-        # has to connect to docker network running on host via docker module 
-        # connect to docker and poke awake container; airflow worker should have docker module installed and access to host socket via volume
-        # TODO: DOCKER_HOST env variable will be used in later versions for ssh connections to remote container instances of scraper container network.
-        # run setUpBrowser from app/src/index.js with proper task argument
-        # wait for confirmation of task completion, forward any logging to airflow logs
-        # mark as success and move on to next task
+    @task(task_id="stop-container")
+    def stop():
         import docker
         client = docker.from_env()
-        scrapingContainer = list(filter(lambda x: x.name=="docker-scraper-1"), client.containers.list())[0]
-        scrapingContainer.exec_run("node ./src/transform.js transform --publix items", user="pptruser", workdir="/app")
+        logs = client.containers.get("scraper").logs(stream=False)
+        logs = logs.decode("ascii")
+        print(logs)
+        client.containers.get("scraper").remove(force=True)
+        print('container stopped')
         return 0
-    # [END kroger_operator_setup_node]
 
-    transformTask = transform_publix_item_data()
-    # [END kroger_operator_transform_api]
 
-    # [START main_flow]
-    wakeUpTask >> extractTask >> transformTask
-    # [END main_flow]
+    send_email = EmailOperator(task_id="send_email_via_operator", to="kylel9815@gmail.com", subject="sent from your docker container...", html_content="""
+            <h1>Hello From Docker !</h1>
+            <h3>just want to inform you that all your tasks from {{run_id}} exited cleanly and the dag run was complete for {{ ts }}.</h3>   
+        """)
+
+    start_container() >> [insertRun("getPublixItems", "get publix's item, pricing and inventory data"), scrape_dataset("publix", "items")] >> updateRun(push=False) >> stop() >> send_email
